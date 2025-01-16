44eb1a7e6f405e102ecde4e79eaeb6ae1c47e468
Arouna AMADOU <a.amadou@bkgspeed.com>
2025-01-16 03:44:37 +0000

    Add allows origins

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: [
+      'https://caisse-menu-depense.web.app',
+      'https://compta-tgcom.web.app',
+    ],
+  });
   app.setGlobalPrefix('api/v1');
   await app.listen(process.env.PORT ?? 3000, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
